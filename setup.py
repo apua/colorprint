e763e1fdbd7a7b38b3119c83381607d6f0deb50a
Apua <Apua.A.Aa@gmail.com>
2015-01-17 23:18:54 +0800

    New architecture. Not test yet

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-from colorprint import (__version__ as version,
-                        __author__ as author)
-
 from distutils.core import setup
 import sys
 
-if sys.version_info[:2] < (3, 4):
-    sys.exit('virtualenv requires Python 3.4 or higher.')
-
-with open('README.rst') as file:
-    long_description = file.read()
-
 setup(
     name='VT100-ColorPrint',
-    version=version,
-    description='Color Print - VT100 Print Functions',
-    long_description=long_description,
-    author=author,
+    version='2',
+    description='Color print functions and command line tool',
+    long_description=open('README.rst').read(),
+    author='Apua',
     url='https://github.com/apua/colorprint',
     license='WTFPL',
-    py_modules=['colorprint'],
+    package_dir={'': 'PY2' if sys.version_info[0]==2 else ''}
+    packages=['colorprint'],
+    scripts=['scripts/colorprint'],
     )
