e072aa21a3f436afbbabd45d3922a1fcf7911403
Apua <Apua.A.Aa@gmail.com>
2015-01-30 20:30:14 +0800

    Update commands

--- colorprint/commands.py
+++ colorprint/commands.py
@@ -1,18 +1,40 @@
-from .info import __version__
-
-
 def get_arguments():
+    '''
+
+    issue: use color value instead of color name ?
+
+    return Namespace object: {
+        color: color name
+        deliver: True or False
+        separator: re.compile object
+        conditions: (
+            {func: color_by_field,
+             fields: {1, -1, slice(2,None,2)},
+             colors: (color names)},
+            {func: color_by_pattern,
+             pattern: re.compile object
+             groups: {...},
+             colors: (color names)},
+            {}, ...
+            )
+    '''
     import argparse
+
+    from .info import __version__
+    from .attributes import attr_names
+
     parser = argparse.ArgumentParser(
         prog='ColorPrint',
         usage='...'*30,
         description='...(give example and how conditions work)',
         epilog='...'*30,
         )
+    subparser = parser.add_subparsers(help='= =').add_parser('show')
+
     parser.add_argument(
         '-F', '--fields', metavar='condition',
         dest='conditions', nargs='+', action='append',
-        #assertion=None,
+        type=type('F',(str,),{}),
         help='...'*30,
         )
     parser.add_argument(
@@ -23,11 +45,12 @@ def get_arguments():
     parser.add_argument(
         '-P', '--pattern', metavar='condition',
         dest='conditions', nargs='+', action='append',
+        type=type('P',(str,),{}),
         help='...'*30,
         )
     parser.add_argument(
         '-C', '--color', metavar='color',
-        nargs='+',
+        nargs='+', #default=get_default_color(),
         help='...'*30,
         )
     parser.add_argument(
@@ -39,7 +62,22 @@ def get_arguments():
         '--version',
         action='version', version=__version__,
         )
-    return parser.parse_args()
+
+    subparser.add_argument(
+        '--16', metavar='attr',
+        nargs='*',
+        type=int,
+        help='...'*30,
+        )
+    subparser.add_argument(
+        '--256', metavar='attr',
+        nargs='*',
+        type=int,
+        help='...'*30,
+        )
+
+    ns = parser.parse_args()
+    return ns
 
 
 def run_cmd():
