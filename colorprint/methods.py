4d3164d1feb6d91069162d1279faf70069491a0e
Apua <Apua.A.Aa@gmail.com>
2015-03-23 15:41:40 +0800

    Reimplemented `pprint_` with `io.TextIOWrapper`

--- colorprint/methods.py
+++ colorprint/methods.py
@@ -25,14 +25,13 @@ class ColorPrint:
             raise TypeError('Do not set colors in both attributes and parameters way')
         elif self.vt_attr:
             coloring = colorform(self.vt_attr).format
-            values_ = map(coloring, map(str, values))
         elif 'colors' in kwargs:
             vt_attr = sum((color_attr_mapping[k] for k in kwargs['colors']), ())
             coloring = colorform(vt_attr).format
-            values_ = map(coloring, map(str, values))
         else:
-            values_ = values
+            coloring = lambda x:x
 
+        values_ = map(coloring, map(str, values))
         locals_ = locals()
         kwargs_ = {key: locals_[key] for key in ('sep','end','file','flush') if locals_[key] is not None}
 
@@ -46,29 +45,46 @@ class ColorPrint:
 
 
 class ColorPPrint(ColorPrint):
-    def __call__(self, object, stream=None, indent=1, width=80, depth=None, *,
-                 compact=False):
+    def __call__(self, values, stream=None, indent=1, width=80, depth=None, *, compact=False, **kwargs):
         """copy from `pprint.pprint`"""
-        from pprint import PrettyPrinter
+        import io
+        import pprint
         import re
 
-        printer = PrettyPrinter(stream=stream, indent=indent,
-                                width=width, depth=depth, compact=compact)
-        stream  = printer._stream
-        color_stream = type('',(),{})()
-        printer._stream = color_stream
+        class ColoringTextIOWrapper(io.TextIOWrapper):
+            def __init__(self, *a, coloring=lambda x:x, **kw):
+                super().__init__(*a, **kw)
+                self.patt = re.compile(r'^(,?)(\n\ *)(.*)$')
+                self.coloring = coloring
 
-        colored = colorform(self.vt_attr).format
-        #color_stream.write = lambda s: stream.write(colored(s))
-        def write(s, patt=re.compile(r'^(,?)(\n\ *)(.*)$')):
-            m = patt.match(s)
-            if m is not None:
-                a, b, c = m.groups()
-                stream.write((a and colored(a)) + b + (c and colored(c)))
-            else:
-                stream.write(colored(s))
-        color_stream.write = write
-        printer.pprint(object)
+            def write(self, s):
+                m = self.patt.match(s)
+                if m is not None:
+                    a, b, c = m.groups()
+                    super().write((a and self.coloring(a)) + b + (c and self.coloring(c)))
+                else:
+                    super().write(self.coloring(s))
+
+        try:
+            invalid_key = next(k for k in kwargs if k!='colors')
+            raise TypeError("'%s' is an invalid keyword argument for this function" % invalid_key)
+        except StopIteration:
+            pass
+
+        if self.vt_attr and 'colors' in kwargs:
+            raise TypeError('Do not set colors in both attributes and parameters way')
+        elif self.vt_attr:
+            coloring = colorform(self.vt_attr).format
+        elif 'colors' in kwargs:
+            vt_attr = sum((color_attr_mapping[k] for k in kwargs['colors']), ())
+            coloring = colorform(vt_attr).format
+        else:
+            coloring = lambda x:x
+
+        printer = pprint.PrettyPrinter(stream=stream, indent=indent, width=width, depth=depth, compact=compact)
+        printer._stream = ColoringTextIOWrapper(printer._stream.buffer, coloring=coloring)
+
+        return printer.pprint(values)
 
 
 print_  = ColorPrint()
