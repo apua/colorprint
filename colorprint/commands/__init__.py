68badc964dd0cffaebf3e91a6b35cf7cbabaa83e
Apua <Apua.A.Aa@gmail.com>
2015-04-19 19:58:25 +0800

    Moved all color information output logic out of __init__.py

--- colorprint/commands/__init__.py
+++ colorprint/commands/__init__.py
@@ -1,23 +1,15 @@
 from .parse_args import create_parser
-from .show_colors import (
-    format_color as to_code,
-    COLOR16,
-    COLOR256,
-    )
+from .show_colors import gen_color_info
 from .coloring import colorprint
 
 
 def run_cmd():
     parser = create_parser()
     args = parser.parse_args()
-    attributes = args.show16 or args.show256
 
-    if   args.show16=='':
-        parser.exit(status=0, message=COLOR16)
-    elif args.show256=='':
-        parser.exit(status=0, message=COLOR256)
-    elif attributes:
-        parser.exit(status=0, message=to_code(attributes))
+    if not (args.show16 is args.show256 is None):
+        color_info = gen_color_info(args.show16, args.show256)
+        parser.exit(status=0, message=color_info)
     elif args.conditions is not None:
         try:
             colorprint(args)
