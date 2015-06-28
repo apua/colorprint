5ab4c20c517bc39b369d6b5ea3e1765f9faf7a56
Apua <Apua.A.Aa@gmail.com>
2015-06-29 00:53:24 +0800

    Remove PY2 legacy since not support yet

---
+++ PY2/README.rst
@@ -0,0 +1,3 @@
+This package is build for Python3 mainly.
+
+It will support Python2 but have no idea how to do it yet.
--- PY2/colorprint/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function
-from .methods import print, pprint
-
-__author__ = 'Apua'
-__version__ = '2'
